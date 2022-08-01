2f81dce113e602c3b4e15d40fbe561b2aa136f6c
gtithathonor <jeong.suyoung92@gmail.com>
2022-08-01 19:48:58 +0900

    강의 연습용 프로젝트 작성 - 회원 등록 및 목록 페이지 연결

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -4,8 +4,11 @@ import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -22,14 +25,21 @@ public class MemberController {
         return "members/createMemberForm";
     }
 
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
 
-//    @PostMapping(value = "/members/new")
-//    public String create(MemberForm form) {
-//        Member member = new Member();
-//        member.setName(form.getName());
-//
-//        memberService.join(member);
-//
-//        return "redirect:/";
-//    }
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
